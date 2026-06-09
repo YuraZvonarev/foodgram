df28cf7c8f226797286bbe796adb6f739d03b2f0
Yura Zvonarev <zvonarov.2003@mail.ru>
2026-06-09 20:46:05 +0300

    fix

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -144,6 +144,7 @@ REST_FRAMEWORK = {
 }
 
 DJOSER = {
+    'LOGIN_FIELD': 'email',
     'SERIALIZERS': {
         'user_create': 'api.serializers.UserCreateSerializer',
         'user': 'api.serializers.UserSerializer',
